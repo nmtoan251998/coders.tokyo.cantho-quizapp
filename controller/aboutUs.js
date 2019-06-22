c770fc9163b76e7c5b47c0d09019a85ba1b5e551
nghipq <nghipqce140179@fpt.edu.vn>
2019-06-22 21:26:52 +0700

    complete page

--- controller/aboutUs.js
+++ controller/aboutUs.js
@@ -1,3 +1,9 @@
+const data = require('../models/fakeTeamLeader');
+const stringMakeUp = require('../function/string-interaction')
+
 module.exports.getAboutUs = (req, res) => {
-    res.render('pages/aboutUs')
+    let teamLeaderData = [...data];
+    teamLeaderData.forEach(item => item.name = stringMakeUp.upperFirstCase(item.name))
+
+    res.render('pages/aboutUs', { teamLeaders: teamLeaderData });
 }
