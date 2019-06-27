ec5144124aa9aef2491c4aba536dafc5325f0279
nghipq <nghipqce140179@fpt.edu.vn>
2019-06-27 07:33:52 +0700

    fix 'what did we do'

---
+++ public/dist.1/owl.carousel.oneSlide.js
@@ -0,0 +1,15 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
