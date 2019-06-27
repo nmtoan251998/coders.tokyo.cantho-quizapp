df78028a09d513155d9c6d9f52977b0c20f0831c
nghipq <nghipqce140179@fpt.edu.vn>
2019-06-27 07:47:23 +0700

    fix component

--- controller/aboutUs.js
+++ controller/aboutUs.js
@@ -1,10 +1,9 @@
 const data = require('../models/fakeTeamLeader');
-const textContents = require('../models/aboutUsTextContents');
 const stringMakeUp = require('../function/string-interaction')
 
 module.exports.getAboutUs = (req, res) => {
     let teamLeaderData = [...data];
     teamLeaderData.forEach(item => item.name = stringMakeUp.upperFirstCase(item.name))
 
-    res.render('pages/aboutUs', { teamLeaders: teamLeaderData, textContents: textContents});
+    res.render('pages/aboutUs', { teamLeaders: teamLeaderData});
 }
